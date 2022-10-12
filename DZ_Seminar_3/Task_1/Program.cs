d4d7570454c622bc9304bebcb2ee67b30dd96597
Tmirskaya <twodragons@mail.ru>
2022-10-12 20:56:32 +0300

    Task1

---
+++ DZ_Seminar_3/Task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, 
+// которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+bool Palindrome(int digit)
+{
+    if (digit/10000 != digit%10) return false;
+    else if ((digit%10000)/1000 != (digit%100)/10) return false;
+    else return true;
+}
+
+Console.WriteLine("Здравствуйте");
+Console.Write("Напишите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 10000 | number > 99999) Console.WriteLine("Это число не является пятизначным!");
+   else if(Palindrome(number) == true) Console.WriteLine("Да");
+           else Console.WriteLine("Нет");
+
+
+
+
+
+
+
+
+
