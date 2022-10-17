5b0c65e9315a1973888efaddc3fd70252a1a79de
Tmirskaya <twodragons@mail.ru>
2022-10-17 18:46:40 +0300

    Task2_4

---
+++ DZ_Seminar_4/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, 
+// которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNumbers(int digit)
+{
+    int sum = 0;
+    while (digit > 0)
+    {
+        sum += digit % 10;
+        digit /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Здравствуйте!");
+Console.Write("Напишите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <0) number *= -1;
+
+Console.WriteLine($"Сумма цифр в числе = {SumNumbers(number)}!");
