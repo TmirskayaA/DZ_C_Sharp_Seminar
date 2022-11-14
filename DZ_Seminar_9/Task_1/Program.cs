74747925fbac598695cd00ccd30a95807481d4a6
Tmirskaya <twodragons@mail.ru>
2022-11-14 21:56:45 +0300

    task1_sem9

---
+++ DZ_Seminar_9/Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void ReturnAllNaturalNumbers(int n)
+{
+    if(n>0)
+    {
+        Console.Write($"{n} ");
+        ReturnAllNaturalNumbers(n-1);
+    }
+    else return;
+}
+
+Console.WriteLine("Здравствуйте!");
+
+Console.Write("До какого числа надо вывести все натуральные числа? ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write($"Вывожу: ");
+ReturnAllNaturalNumbers(number);
