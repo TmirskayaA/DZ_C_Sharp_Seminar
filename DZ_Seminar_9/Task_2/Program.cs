c0144f8e06bb65f9ccecd1a6310255bd3c43ce3c
Tmirskaya <twodragons@mail.ru>
2022-11-14 22:06:50 +0300

    task2_sem9

---
+++ DZ_Seminar_9/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, 
+// которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNaturalNumbers(int min, int max)
+{
+    if(min < max) return min + SumNaturalNumbers(min+1, max);
+    else return max;
+}
+
+Console.WriteLine("Здравствуйте!");
+
+Console.Write("От какого числа считаем сумму? ");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.Write("До какого числа считаем сумму? ");
+int maximum = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write($"Сумма чисел = {SumNaturalNumbers(minimum, maximum)}");
